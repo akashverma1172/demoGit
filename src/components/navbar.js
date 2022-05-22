f6f764800337650715663fa39ca1630447a65ca8
AVerma <averma@ee.iitr.ac.in>
2022-05-22 15:47:14 +0530

    CornerStore basic setup

---
+++ src/components/navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Navbar(){
+    return(
+        <div className="nav-container">
+            <img className="nav-logo-img" src="./media/bucket.png"/>
+            <p className="nav-logo">CornerStore</p>
+            <ul className="nav-list">
+                <li className="nav-item"><a href="#">Home</a></li>
+                <li className="nav-item"><a href="#">Products</a></li>
+                <li className="nav-item"><a href="#">Services</a></li>
+                <li className="nav-item"><a href="#">Contact Us</a></li>
+            </ul>
+            <img className="nav-user" src="./media/download.png" />
+        </div>
+    );
+}
+
+export default Navbar;
