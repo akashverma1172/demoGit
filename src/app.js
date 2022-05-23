5e3a5ac76177250778dca1e7c0116601f1ec15c0
AVerma <averma@ee.iitr.ac.in>
2022-05-23 15:44:16 +0530

    added Courses

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 import React from "react";
 import Navbar from "./components/navbar";
+import CourseCard from "./components/courseCard";
+import courses from "./components/course_data";
 
 function App(){
+    const course_list = courses.map((course)=>{
+        return <CourseCard
+                name = {course.name}
+            />
+    });
+    console.log(course_list);
     return(
-        <Navbar/>
+        <div className="app-container">
+            <Navbar />
+            {course_list}
+        </div>
     );
 }
 
---
+++ src/components/course_data.js
@@ -0,0 +1,49 @@
+const courses = [
+    {
+        id:1,
+        name: "Beginning Django_ Web Application Development and Deployment with Python",
+        category: ["Web Development","Python"],
+        releaseYear: 2017,
+        rating: 4.3
+    },
+    {
+        id:2,
+        name: "books-agiliq-com-django-api-polls-tutorial-en-latest",
+        category: ["Web Development","Python", "API"],
+        releaseYear: 2019,
+        rating: 4.1
+    },
+    {
+        id:3,
+        name: "Data Analysis From Scratch With Python_ Beginner Guide using Python, Pandas, NumPy, Scikit-Learn, IPython, TensorFlow and Matplotlib",
+        category: ["Machine Learning","Python", "Tensorflow",],
+        releaseYear: 2016,
+        rating: 4.9
+    },
+    {
+        id:4,
+        name: "Deep Learning with Applications Using Python",
+        category: ["Deep Learning","Python"],
+        releaseYear: 2021,
+        rating: 4.4
+    },
+    {
+        id:5,
+        name: "Flask Web Development_ Developing Web Applications with Python",
+        category: ["Web Development", "Flask","Python"],
+        releaseYear: 2021,
+        rating: 3.8
+    }
+]
+
+export default courses;
+
+/*
+    {
+        id:,
+        name: "",
+        category: ["","Python"],
+        releaseYear: 20,
+        rating: 4.,
+    },
+*/
